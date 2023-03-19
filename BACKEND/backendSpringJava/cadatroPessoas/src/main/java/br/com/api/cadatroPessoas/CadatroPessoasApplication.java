5ee9415fb82581b571245bae65b94dbc7234636d
Rafael <rafael.cmota@hotmail.com>
2023-03-18 23:54:57 -0300

    Adicionando projeto backend spring boot java

---
+++ BACKEND/backendSpringJava/cadatroPessoas/src/main/java/br/com/api/cadatroPessoas/CadatroPessoasApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.cadatroPessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadatroPessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadatroPessoasApplication.class, args);
+	}
+
+}
---
+++ BACKEND/backendSpringJava/cadatroPessoas/src/test/java/br/com/api/cadatroPessoas/CadatroPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.cadatroPessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadatroPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
