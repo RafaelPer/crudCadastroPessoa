27bcb80c415152e0b8f9af0e09a15aaa1df07b84
Rafael <rafael.cmota@hotmail.com>
2023-03-21 23:11:18 -0300

    adicionando atributos e configurando a criação de tabela do model parceiro negocio

--- BACKEND/backendSpringJava/cadatroPessoas/src/main/java/br/com/api/cadatroPessoas/models/ParceiroNegocio.java
+++ BACKEND/backendSpringJava/cadatroPessoas/src/main/java/br/com/api/cadatroPessoas/models/ParceiroNegocio.java
@@ -1,5 +1,18 @@
 package br.com.api.cadatroPessoas.models;
 
-public class parceiroNegocio {
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = ParceiroNegocio.TABLE_NAME)
+public class ParceiroNegocio {
+	public static final String TABLE_NAME = "parceiroNegocio";
+	
+	
+	@OneToMany(mappedBy = "parceiroNegocio")
+    private Set<Usuario> usuarios;
 
 }
