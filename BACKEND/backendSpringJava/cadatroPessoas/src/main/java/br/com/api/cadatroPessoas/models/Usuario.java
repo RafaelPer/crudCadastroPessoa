4feb682965197beb4119bc6bb46a9450b0dd8c1d
Rafael <rafael.cmota@hotmail.com>
2023-03-21 23:07:22 -0300

    adicionando atributos e configurando a criação de tabela do model usuario

--- BACKEND/backendSpringJava/cadatroPessoas/src/main/java/br/com/api/cadatroPessoas/models/Usuario.java
+++ BACKEND/backendSpringJava/cadatroPessoas/src/main/java/br/com/api/cadatroPessoas/models/Usuario.java
@@ -1,5 +1,34 @@
 package br.com.api.cadatroPessoas.models;
 
-public class usuario {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = Usuario.TABLE_NAME)
+public class Usuario {
+	public static final String TABLE_NAME = "usuario";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idUsuario", nullable = false, unique = true)
+	private Long idUsuario;
+	
+	@Column(name = "nome", nullable = false, unique = true, length = 60)
+	private String nome;
+	
+	@Column(name = "senha", nullable = false, length = 255)
+	private String senha;
+	
+    @ManyToOne
+    @JoinColumn(name="idParceiroNegocioFK", nullable=false)
+    private ParceiroNegocio idParceiroNegocioFK;
+	
+	
 }
+	
